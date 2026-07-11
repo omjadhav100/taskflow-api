3d3c74ef438c92be90830d5cb284676d187bc85e
Om Jadhav <YOUR_GITHUB_NOREPLY_EMAIL>
2026-07-11 14:25:31 +0530

    Day 1: Hello World Spring Boot endpoint

---
+++ src/main/java/com/taskflow/taskflow_api/HelloController.java
@@ -0,0 +1,13 @@
+package com.taskflow.taskflow_api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "TaskFlow API is alive!";
+    }
+}
